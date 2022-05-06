46d39cfa1b7b7afda98f8291c6a4c40f7d57b64c
Hunterbeachum <hsbeachum@my.waketech.edu>
2022-05-06 19:02:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from flask import Flask
 from waitress import serve
+import random
 import requests
 import json
 
 
-
 app = Flask(__name__)
-api_key = '4cd4f08b-4233-47cd-90b1-cffdb5ac5d8a'
+api_key_cats = '4cd4f08b-4233-47cd-90b1-cffdb5ac5d8a'
 
 
+# CHANGED THIS
 @app.route('/')
-def hello():
+def home():
     payload = {}
     r = requests.get('https://api.thecatapi.com/v1/images/search', params=payload)
     print('URL: ', r.url)
@@ -18,6 +19,22 @@ def hello():
     url = cat_data[0]['url']
     html = f'<img src="{url}" alt="A random cat image">'
     return html
+
+
+@app.route('/movie')
+def movie():
+    payload = {}
+    a_random_int = random.randint(2,1000)
+    v = requests.get(f'https://api.themoviedb.org/3/movie/{a_random_int}?api_key=29e35d1a18f27c1185d11c046faff565', params=payload)
+    movie_data2 = json.loads(v.text)
+    while 'success' in movie_data2:
+        a_random_int = random.randint(2,1000)
+        v = requests.get(f'https://api.themoviedb.org/3/movie/{a_random_int}?api_key=29e35d1a18f27c1185d11c046faff565')
+        movie_data2 = json.loads(v.text)
+    movie_title = movie_data2['original_title']
+    html = f'Your random movie to watch is {movie_title}'
+    return html
+
 
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=8080)
